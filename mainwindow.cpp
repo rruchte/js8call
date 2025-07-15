de72d8e4f1b69b583fd09a2761cd4bad8237d93b
rruchte <rob@thirdpartylabs.com>
2025-07-15 18:09:35 -0400

    Changes to MainWindow::tryBandHop() - Explicitly set schedule window start and end times to UTC. - Extend endOfDay time to cover the final minute of the day. - Fix typos in comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1831,15 +1831,20 @@ void MainWindow::tryBandHop(){
   d.setDate(QDate(2000, 1, 1));
 
   QDateTime startOfDay = QDateTime(QDate(2000, 1, 1), QTime(0, 0));
-  QDateTime endOfDay = QDateTime(QDate(2000, 1, 1), QTime(23, 59));
+  QDateTime endOfDay = QDateTime(QDate(2000, 1, 1), QTime(23, 59, 59, 999));
 
   // see if we can find a needed band switch...
   foreach(auto station, stations){
       // we can switch to this frequency if we're in the time range, inclusive of switch_at, exclusive of switch_until
       // and if we are switching to a different frequency than the last hop. this allows us to switch bands at that time,
       // but then later we can later switch to a different band if needed without the automatic band switching to take over
+
+	  // Ensure that the timezones are UTC
+	  station.switch_at_.setTimeZone(QTimeZone::utc());
+	  station.switch_until_.setTimeZone(QTimeZone::utc());
+
       bool inTimeRange = (
-        (station.switch_at_ <= d && d <= station.switch_until_) ||          // <- normal range, 12-16 && 6-8, evalued as 12 <= d <= 16 || 6 <= d <= 8
+        (station.switch_at_ <= d && d <= station.switch_until_) ||          // <- normal range, 12-16 && 6-8, evaluated as 12 <= d <= 16 || 6 <= d <= 8
 
         (station.switch_until_ < station.switch_at_ && (                    // <- say for a range of 12->2 & 2->12;  12->2,
              (station.switch_at_ <= d && d <= endOfDay)         ||          //    should be evaluated as 12 <= d <= 23:59 || 00:00 <= d <= 2
